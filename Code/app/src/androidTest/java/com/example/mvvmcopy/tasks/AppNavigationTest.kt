b00bc2b291a4820835437ab7fd07b9e688e70e7d
Daniel <daniel@karaokesmart.co>
2019-09-03 18:24:23 -0500

    Wip: Generated Files

--- Code/app/src/androidTest/java/com/example/mvvmcopy/tasks/AppNavigationTest.kt
+++ Code/app/src/androidTest/java/com/example/mvvmcopy/tasks/AppNavigationTest.kt
@@ -1,8 +1,13 @@
 package com.example.mvvmcopy.tasks
 
+import androidx.test.filters.LargeTest
+import androidx.test.runner.AndroidJUnit4
 import com.example.mvvmcopy.DaggerTestApplicationRule
 import org.junit.Rule
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
+@LargeTest
 class AppNavigationTest {
 
     @get:Rule
